7eb7eb79122a8afa0f3aafeb95d47b4e44062f73
Leon Chisum <leonchisum3306@gmail.com>
2020-05-18 13:38:27 -0400

    TEST - Seeding - added data seeds to DB for guards

--- routes/guardsRouter.js
+++ routes/guardsRouter.js
@@ -4,14 +4,34 @@ const Guard = require("../models/employee_guard");
 const faker = require("faker");
 
 const auth = require("../middleware/auth");
-const { seedGuards } = require('../models/seeds/seeds')
+const { seedGuards } = require("../models/seeds/seeds");
 
 // @Route POST /guards
 // @desc create new guard
 // @acess Private
 router.post("/", auth, async (req, res) => {
   try {
-    const { firstName, lastName, ssn } = req.body;
+    const {
+      firstName,
+      lastName,
+      email,
+      address: { street, city, state },
+      phone: { cell, home },
+      ssn,
+      birthDate,
+      startDate,
+      dLicense: { blueCard, number, exp },
+      gLicense: { activeLicense, number, exp },
+      ccw: { activeLicense, number, exp },
+      uniform: {
+        polo: { hasIssued, size },
+        jacket: { hasIssued, size },
+      },
+      badge: { hasIssued, barcode },
+      position,
+      rating,
+      shiftPref: { startTime, endTime },
+    } = req.body;
 
     //validation
     if (!firstName || !lastName)
@@ -22,70 +42,61 @@ router.post("/", auth, async (req, res) => {
 
     if (guard) return res.status(400).json({ message: "Guard already exists" });
 
-    const guards = await seedGuards(50);
-
-    console.log(guards)
-
-    Guard.insertMany(guards, (err, docs) => {
-        if(err) return console.log(err)
-        res.status(200).json({ guardsAdded: docs})
-    })
-
     //Creating new guard
-    // const newAdmin = new Guard({
-    //   firstName,
-    //   lastName,
-    //   email,
-    //   address: {
-    //     street,
-    //     city,
-    //     state: "FL",
-    //   },
-    //   phone: {
-    //     cell,
-    //     home,
-    //   },
-    //   ssn,
-    //   birthDate,
-    //   startDate,
-    //   dLicense: {
-    //     blueCard,
-    //     number,
-    //     exp,
-    //   },
-    //   gLicense: {
-    //     activeLicense,
-    //     number,
-    //     exp,
-    //   },
-    //   ccw: {
-    //     activeLicense,
-    //     number,
-    //     exp,
-    //   },
-    //   uniform: {
-    //     polo: {
-    //       hasIssued,
-    //       size
-    //     },
-    //     jacket: {
-    //         hasIssued,
-    //         size
-    //       },
-    //   },
-    //   badge : {
-    //       hasIssued,
-    //       barcode,
-    //   },
-    //   position,
-    //   rating,
-    //   shiftPref: {
-    //       startTime,
-    //       endTime,
-    //   }
-    // });
+    const newAdmin = new Guard({
+      firstName,
+      lastName,
+      email,
+      address: {
+        street,
+        city,
+        state
+      },
+      phone: {
+        cell,
+        home,
+      },
+      ssn,
+      birthDate,
+      startDate,
+      dLicense: {
+        blueCard,
+        number,
+        exp,
+      },
+      gLicense: {
+        activeLicense,
+        number,
+        exp,
+      },
+      ccw: {
+        activeLicense,
+        number,
+        exp,
+      },
+      uniform: {
+        polo: {
+          hasIssued,
+          size,
+        },
+        jacket: {
+          hasIssued,
+          size,
+        },
+      },
+      badge: {
+        hasIssued,
+        barcode,
+      },
+      position,
+      rating,
+      shiftPref: {
+        startTime,
+        endTime,
+      },
+    });
   } catch (error) {
-      console.log(error)
+    console.log(error);
   }
 });
 
