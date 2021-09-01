2bffd55fa08c49a42ed0fe8695bda5ea0843d021
LeonChisum <leonchisum3306@gmail.com>
2021-09-01 11:03:45 -0400

    updated the show dashboard

--- client/src/actions/showActions.js
+++ client/src/actions/showActions.js
@@ -1,20 +1,42 @@
-import { axiosWithAuth } from '../helperFuncs';
-import { SHOWS_LOADED, SHOWS_FAIL, SHOWS_LOADING } from '../actions/types';
-import { returnErrors } from './errorActions';
+import { axiosWithAuth } from "../helperFuncs";
+import {
+  SHOWS_LOADED,
+  SHOWS_FAIL,
+  SHOWS_LOADING,
+  SINGLE_SHOW_LOADED,
+} from "../actions/types";
+import { returnErrors } from "./errorActions";
 
 export const getShows = () => (dispatch) => {
-	// Load show
-	dispatch({ type: SHOWS_LOADING });
+  // Load show
+  dispatch({ type: SHOWS_LOADING });
 
-	// send request to Backend
-	axiosWithAuth()
-		.get('/shows')
-		.then((res) => {
-			console.log(res);
-			dispatch({ type: SHOWS_LOADED, payload: res.data });
-		})
-		.catch((err) => {
-			dispatch(returnErrors(err.response.data.message, err.response.status));
-			dispatch({ type: SHOWS_FAIL });
-		});
+  // send request to Backend
+  axiosWithAuth()
+    .get("/shows")
+    .then((res) => {
+      console.log(res);
+      dispatch({ type: SHOWS_LOADED, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data.message, err.response.status));
+      dispatch({ type: SHOWS_FAIL });
+    });
+};
+
+export const getSelectedShow = (id) => (dispatch) => {
+  // Load show
+  dispatch({ type: SHOWS_LOADING });
+
+  // send request to Backend
+  axiosWithAuth()
+    .get(`/shows/${id}`)
+    .then((res) => {
+      console.log(res);
+      dispatch({ type: SINGLE_SHOW_LOADED, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data.message, err.response.status));
+      dispatch({ type: SHOWS_FAIL });
+    });
 };
